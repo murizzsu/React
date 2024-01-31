7c6ae5e6b400981f1194a7e80907838d21276bd4
murizzsu <rizzkyy2207@gmail.com>
2024-01-31 22:57:43 +0700

    Conditional Rendering

---
+++ Conditional_Rendering/README.md
@@ -0,0 +1,17 @@
+# Conditional Rendering
+- digunakan untuk menampilkan halaman yang berbeda dengan menggunakan kondisi tertentu seperti and, or , if , dll
+- contoh penggunaaan ada di file Auth layout
+```
+<p className='text-sm mt-5 text-center'>
+                    {type === "login" ? " Don't have an account?" : "Already have an account?"}
+                    {type == "login" && (<Link to='/register' className='font-bold text-blue-600'>Register</Link>)}
+                    {type == "register" && (<Link to='/login' className='font-bold text-blue-600'>Login</Link>)}
+
+                </p>
+```
+
+- Pengkondisian ternary digunakan ketika terdapat 2 kondisi
+
+- Komponen bisa digunakan untuk pengkondisian
+- jika komponen masih dalam 1 file yang sama (workspace) maka tidak perlu menggunakan export default. Cukup panggil fungsi seperti authlayout
+
---
+++ Conditional_Rendering/src/components/Layouts/AuthLayouts.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import FormLogin from '../Fragments/FormLogin'
+import { Link } from 'react-router-dom'
+const AuthLayouts = (props) => {
+    const { children, title, type } = props
+    return (
+        <div className="flex justify-center min-h-screen items-center">
+            <div className="w-full max-w-xs">
+                <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
+                <p className="font-medium text-slate-500 mb-8">
+                    Welcome, Please enter your details
+                </p>
+                {children}
+
+                {/* <p className='text-sm mt-5 text-center'>
+                    {type === "login" ? " Don't have an account?" : "Already have an account?"}
+                    {type == "login" && (<Link to='/register' className='font-bold text-blue-600'>{" "}Register</Link>)}
+                    {type == "register" && (<Link to='/login' className='font-bold text-blue-600'>{" "}Login</Link>)}
+                </p> */}
+
+                {<Navigation type={type}/>}
+
+
+            </div>
+        </div>
+    )
+}
+
+const Navigation = ({ type }) => {
+    if (type === "login") {
+        return (
+            <p className='text-sm mt-5 text-center'>
+                Don't have an account? {" "}
+                {type == "login" && (<Link to='/register' className='font-bold text-blue-600'>{" "}Register</Link>)}
+            </p>
+        )
+    } else {
+        return (
+            <p className='text-sm mt-5 text-center'>
+                Already have an account? {" "}
+                {type == "register" && (<Link to='/login' className='font-bold text-blue-600'>{" "}Login</Link>)}
+            </p>
+        )
+    }
+}
+
+export default AuthLayouts
