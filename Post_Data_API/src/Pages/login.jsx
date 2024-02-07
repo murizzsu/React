f922f4a208f42da91c3c1dd56630b3951b35d520
murizzsu <rizzkyy2207@gmail.com>
2024-02-07 22:27:58 +0700

    Get Data From API Using FakeStoreAPI

---
+++ Post_Data_API/src/Pages/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AuthLayouts from '../components/Layouts/AuthLayouts'
+import FormLogin from '../components/Fragments/FormLogin'
+
+const LoginPage = () => {
+  return (
+    <AuthLayouts title="Login" type="login">
+      <FormLogin />
+    </AuthLayouts>
+  )
+}
+
+export default LoginPage
